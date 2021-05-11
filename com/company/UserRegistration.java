988141e26ccd9e1cfc047b54d5959e825ad8d42a
YashPareek1 <yashpareekg4@gmail.com>
2021-05-12 00:45:02 +0530

    adding welcome message

---
+++ com/company/UserRegistration.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+	// write your code here
+
+            System.out.println("Welcome to USER REGISTRATION PROBLEM");
+
+        }
+}
